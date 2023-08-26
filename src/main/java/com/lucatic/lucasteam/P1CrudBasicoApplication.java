43c4229e259a02c33051375608d556b9b8b1a890
Alberto Galarzo <liaburns@hotmail.com>
2023-08-27 00:02:49 +0200

    primer commit

---
+++ src/main/java/com/lucatic/lucasteam/P1CrudBasicoApplication.java
@@ -0,0 +1,13 @@
+package com.lucatic.lucasteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1CrudBasicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1CrudBasicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucatic/lucasteam/P1CrudBasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucatic.lucasteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1CrudBasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
